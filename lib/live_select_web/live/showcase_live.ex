a70c9f0dbfd546680e004849fb4ff6df067e228d
Max Marcon <maxmarcon@gmx.net>
2022-06-20 22:42:14 +0200

    add handle_info

--- lib/live_select_web/live/showcase_live.ex
+++ lib/live_select_web/live/showcase_live.ex
@@ -15,6 +15,17 @@ defmodule LiveSelectWeb.ShowcaseLive do
       </p>
       """
     end
+
+    def handle_info(assigns) do
+      ~H"""
+      <p>
+      def handle_info(
+        <span class="text-success"><%= inspect(@msg) %></span>, 
+        socket
+      )
+      </p>
+      """
+    end
   end
 
   @impl true
@@ -27,21 +38,37 @@ defmodule LiveSelectWeb.ShowcaseLive do
     socket =
       assign(socket,
         change_msg: :change,
+        msg: nil,
+        recent_msg: false,
         cities: cities,
         event: nil,
+        recent_event: false,
         params: nil,
-        recent_event: false
+        form_name: "form"
       )
 
     {:ok, socket}
   end
 
   @impl true
+  def handle_event(
+        "update-settings",
+        %{"settings" => %{"form_name" => form_name, "change_msg" => change_msg}},
+        socket
+      ) do
+    {:noreply, assign(socket, form_name: form_name, change_msg: change_msg)}
+  end
+
+  @impl true
   def handle_event(event, params, socket) do
-    Process.send_after(self(), :clear_recent_event, 2000)
+    Process.send_after(self(), :clear_recent_event, 1_000)
 
     {:noreply,
-     assign(socket, event: event, params: params |> Map.take(["form"]), recent_event: true)}
+     assign(socket,
+       event: event,
+       params: params |> Map.take([socket.assigns.form_name]),
+       recent_event: true
+     )}
   end
 
   @impl true
@@ -50,21 +77,28 @@ defmodule LiveSelectWeb.ShowcaseLive do
   end
 
   @impl true
+  def handle_info(:clear_recent_msg, socket) do
+    {:noreply, assign(socket, :recent_msg, false)}
+  end
+
+  @impl true
   def handle_info(message, socket) do
     change_msg = socket.assigns.change_msg
 
     case message do
-      {^change_msg, text} ->
+      {^change_msg, text} = msg ->
         send_update(LiveSelect,
           id: "live_select",
           options: cities(text, socket.assigns.cities)
         )
 
-      _ ->
-        IO.puts("unknown message")
-    end
+        Process.send_after(self(), :clear_recent_msg, 1_000)
 
-    {:noreply, socket}
+        {:noreply, assign(socket, msg: msg, recent_msg: true)}
+
+      _ ->
+        {:noreply, socket}
+    end
   end
 
   defp cities("", _cities), do: []
