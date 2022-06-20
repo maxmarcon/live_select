253858b2ce305f033784fbb8750b61b7a4d86145
Max Marcon <maxmarcon@gmx.net>
2022-06-20 21:48:36 +0200

    add handle_event component

--- lib/live_select_web/live/showcase_live.ex
+++ lib/live_select_web/live/showcase_live.ex
@@ -1,6 +1,22 @@
 defmodule LiveSelectWeb.ShowcaseLive do
   use LiveSelectWeb, :live_view
 
+  defmodule Util do
+    use Phoenix.Component
+
+    def handle_event(assigns) do
+      ~H"""
+      <p>
+      def handle_event(
+        <span class="text-success"><%= inspect(@event) %></span>, 
+        <span class="text-primary"><%= inspect(@params) %></span>, 
+        socket
+      )
+      </p>
+      """
+    end
+  end
+
   @impl true
   def mount(_params, _session, socket) do
     cities =
@@ -12,37 +28,29 @@ defmodule LiveSelectWeb.ShowcaseLive do
       assign(socket,
         change_msg: :change,
         cities: cities,
-        handle_event: "",
+        event: nil,
+        params: nil,
         recent_event: false
       )
 
     {:ok, socket}
   end
 
-
   @impl true
   def handle_event(event, params, socket) do
-    handle_event = ~s"""
-              handle_event(
-                  #{event}, 
-                  #{inspect(params)}, 
-                  socket
-              )    
-              """
-
     Process.send_after(self(), :clear_recent_event, 2000)
-    
-    {:noreply, assign(socket, handle_event: handle_event, recent_event: true)}
+
+    {:noreply,
+     assign(socket, event: event, params: params |> Map.take(["form"]), recent_event: true)}
   end
-  
-  @impl true 
+
+  @impl true
   def handle_info(:clear_recent_event, socket) do
     {:noreply, assign(socket, :recent_event, false)}
   end
 
   @impl true
   def handle_info(message, socket) do
-    print_message(message)
     change_msg = socket.assigns.change_msg
 
     case message do
@@ -57,10 +65,6 @@ defmodule LiveSelectWeb.ShowcaseLive do
     end
 
     {:noreply, socket}
-  end
-
-  defp print_message(message) do
-    IO.inspect(message, label: "MESSAGE")
   end
 
   defp cities("", _cities), do: []
