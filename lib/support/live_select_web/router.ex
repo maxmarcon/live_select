69e46919aecc358abf80d40f032291eeff498a79
Max Marcon <maxmarcon@gmx.net>
2023-02-07 09:47:28 +0100

    add strict CSP to showcase app

--- lib/support/live_select_web/router.ex
+++ lib/support/live_select_web/router.ex
@@ -7,7 +7,7 @@ defmodule LiveSelectWeb.Router do
     plug :fetch_live_flash
     plug :put_root_layout, {LiveSelectWeb.LayoutView, :root}
     plug :protect_from_forgery
-    plug :put_secure_browser_headers
+    plug :put_secure_browser_headers, %{"content-security-policy" => "default-src 'self'"}
   end
 
   pipeline :api do
