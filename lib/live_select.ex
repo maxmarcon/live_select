3617944329adc9816e186508d1e369e2dcf1554f
Max Marcon <maxmarcon@gmx.net>
2023-06-26 11:34:18 +0200

    update docs

--- lib/live_select.ex
+++ lib/live_select.ex
@@ -319,11 +319,11 @@ defmodule LiveSelect do
 
   attr :"phx-blur", :string,
     doc:
-      "Event to emit when the text input loses focus. The component id will be send in the event's params"
+      "Event to emit when the text input loses focus. The component id will be sent in the event's params"
 
   attr :"phx-focus", :string,
     doc:
-      "Event to emit when the text input is receives focus. The component id will be send in the event's params"
+      "Event to emit when the text input receives focus. The component id will be sent in the event's params"
 
   @styling_options ~w(active_option_class available_option_class container_class container_extra_class dropdown_class dropdown_extra_class option_class option_extra_class text_input_class text_input_extra_class text_input_selected_class selected_option_class tag_class tag_extra_class tags_container_class tags_container_extra_class)a
 
